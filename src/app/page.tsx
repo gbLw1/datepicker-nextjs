8ae911db539bb7af889bbf9646b85f11a5cc9344
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2024-05-29 18:48:09 -0300

    feat: date picker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,43 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import React from "react";
+import { isSameDay } from "date-fns";
+import moment from "moment";
+import { useCalendarStore } from "@/store/calendar.store";
+import { CustomCalendar } from "@/components/calendar";
+import { sessions } from "@/data/sessions";
 
 export default function Home() {
+  const { date } = useCalendarStore();
+
+  const sessionsAvailable: string[] = sessions.filter((session) => {
+    const sessionDate = new Date(session);
+    return isSameDay(date!, sessionDate);
+  });
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <CustomCalendar sessions={sessions} />
+      <h1>Selected date: {date && moment(date).format("DD/MM/YYYY")}</h1>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <p>
+        Sessions available:{" "}
+        {sessionsAvailable.length > 0 ? (
+          <ul>
+            {sessionsAvailable.map((session, index) => (
+              <li key={index} className="mb-2">
+                <button className="btn btn-primary">
+                  {moment(session).format("HH[h]mm")}
+                </button>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <strong>
+            <em>none</em>
+          </strong>
+        )}
+      </p>
+    </>
   );
 }
